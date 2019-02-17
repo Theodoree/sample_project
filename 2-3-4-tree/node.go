82e7c761fe978a78897751a88034734bbe43c78b
Ted <xinkun.wan@xibao100.com>
2019-02-17 14:36:48 +0800

    sync

--- 2-3-4-tree/node.go
+++
@@ -1,1 +0,0 @@
-package __3_4_tree
--- main.go
+++ main.go
@@ -6,8 +6,26 @@ import (
 
 func main(){
 
-
-	var a float64
-	a = 999999999.999999999
+	a:=GetPercentList([]int{5169,17,30,13,6,2,6,2,6,12,2,2,1,1,2,2,1})
 	fmt.Println(a)
+	for _,value:=range a{
+		fmt.Print(value,",")
+	}
+}
+
+
+func GetPercentList(valueList []int)(PercentList []string){
+	var total float64
+	for _,value:=range valueList{
+		total+=float64(value)
+	}
+
+	for _,value:=range valueList{
+		percent:=float64(value)/total
+		fmt.Println(percent)
+		PercentList = append(PercentList,fmt.Sprintf("%.2f ",percent*100))
+	}
+
+
+	return
 }
